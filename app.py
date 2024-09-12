f52044b59b0bb860b50f4158d8da1a5ec352f93e
balad06 <d.balasubramanian006@gmail.com>
2024-09-12 10:17:55 +0200

    packages

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+import numpy as np
+import tensorflow
+import keras
+from tensorflow import keras
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+import json
+import pickle
+import nltk
+from nltk.stem import WordNetLemmatizer
+
 def main():
     print("Hello World!")
 
