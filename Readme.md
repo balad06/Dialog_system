aadc6cf1f8c34fd66b13e54f81da32806242fc75
balad06 <d.balasubramanian006@gmail.com>
2024-09-26 09:35:43 +0200

    working dialog system

--- Readme.md
+++ Readme.md
@@ -17,3 +17,15 @@ The aim of this repository is to create a working dialog system along with class
     2) Rule based: Accuracy .77
     3) Logistic regression: Accuracy .92
     4) Decision Tree: Accuracy .89 
+
+1b Dialog system:
+    In this part we have implement a dialog system to enable the user to search for restaurants in the city
+
+    Implementations
+
+    1) We have used logistic regression to classify the users text input after converting the messages to lower cases.However due to the varied ways user can type messages we are processing the dialog acts hi, inform ,no dialog(None) , requests.
+    2) After this we remove the stop words using spacy in the message to extract keywords using keybert(the best performing among yake-NLTK,Rake).
+    3) We use these keywords to identify the restaurants specified by the user in the csv file provide and return a random restaurant from the selection.
+    4) If the system is not able to find an exact match it uses the levenshtein distance to provide the closest value.
+    5) We store the selected restaurant and the filtered results as a CSV file to help the user with further questions and providing with alternative response
+    6) Yet to be implemented!! use the results stored and the next message of the user to continue the conversation and find the exact result!
--- appchatbot.py
+++ appchatbot.py
@@ -1,8 +1,10 @@
 import pickle
-from rake_nltk import Rake
 import nltk
 from keybert import KeyBERT
 import pandas as pd
+import spacy
+import os
+import Levenshtein
 class Chatbot:
     def textclassfier(self,msg):
         with open(r'raw_files/logisticmodelnodupe.pkl', 'rb') as f:
@@ -13,50 +15,113 @@ class Chatbot:
         result=lr.predict(msg_cv)
         return result
     def prepresponse():
+        #Use for preparping responses for diffrent dialog acts
         return 'text'
     def keyextractions(self,msg):
-        # text = "Your text here"
-        # r = Rake()
-        # r.extract_keywords_from_text(msg)
-        # keywords = r.get_ranked_phrases()
-        kw_model = KeyBERT()
+        nlp = spacy.load("en_core_web_sm")#REmove stop words
         text = msg
-        keywords = kw_model.extract_keywords(text)
-# print(keywords) 
+        doc = nlp(text)
+        filtered_words = [token.text for token in doc if not token.is_stop]
+        clean_text = ' '.join(filtered_words)
+        kw_model = KeyBERT()
+        keywords = kw_model.extract_keywords(clean_text)
         return keywords
-    def distance():
-        return 'distance'
-    def get_results(self,message,intent,keywords):
+    def levenshtein_distance(self,value, target):
+        if isinstance(value, str):
+            return Levenshtein.distance(value, target)
+        else:
+            return None
+    
+    def closest_results(self,keywords):
         df =pd.read_csv(r'raw_files/restaurant_info.csv')
-        # df.index.name='restaurantname'
-        matched_restarent =pd.DataFrame()
         for keyword in keywords:
             keyword = keyword[0]
-            for column in df.columns:
-                matches = df[df[column].str.contains(keyword, case=False, na=False)]
-                if not matches.empty:
-                    matched_restarent = pd.concat([matched_restarent, matches])
+            #to avoid unwanted loops
+            if keyword!= 'hotels' and keyword!='restaurants' and keyword!= 'hotel' and keyword!='restaurant' and keyword!='food':
+                for column in ['pricerange','area','food']:
+                    df['lv_{}'.format(column)] = df[column].apply(lambda x: self.levenshtein_distance(x, keyword))
+        df['lvDistance'] = df[['lv_pricerange', 'lv_area', 'lv_food']].min(axis=1)
+        min = df['lvDistance'].min()
+        df['min_dist'] = df[['lv_pricerange', 'lv_area', 'lv_food']].idxmin(axis=1)
+        df.drop(['lv_pricerange', 'lv_area', 'lv_food'], axis=1, inplace=True)
+        if min<5.0:
+            closest_restaurants = df[df['lvDistance'] == min]
+            closest_restaurants.to_csv(r'raw_files/closest_results.csv',index=False)
+            return closest_restaurants
+        else:
+            return  pd.DataFrame(columns=df.columns)
+    def get_results(self,message,intent,keywords):
+        df =pd.read_csv(r'raw_files/restaurant_info.csv')
+        matched_restarent =pd.DataFrame(columns=df.columns)
+        columns=[]
+        if intent in ['hi','inform','no dialog','request']:
+            for keyword in keywords:
+                keyword = keyword[0]
+                #to avoid unwanted loops
+                if keyword!= 'hotels' and keyword!='restaurants' and keyword!= 'hotel' and keyword!='restaurant' and keyword!='food':
+                    for column in ['pricerange','area','food']:
+                        matches = df[df[column].str.contains(keyword, case=False, na=False)]
+                        if not matches.empty:
+                            if matched_restarent.empty:
+                                matched_restarent=matches
+                                columns.append(column)
+                            else:
+                                columns.append(column)
+                                matches = matches.rename(columns={'restaurantname_x': 'restaurantname', 'area_x': 'area','pricerange_x':'pricerange','food_x':'food','addr_x':'addr','postcode_x':'postcode'})
+                                matched_restarent = matched_restarent.rename(columns={'restaurantname_x': 'restaurantname', 'area_x': 'area','pricerange_x':'pricerange','food_x':'food','addr_x':'addr','postcode_x':'postcode'})
+                                matched_restarent = pd.merge(matched_restarent, matches, how='inner',on=['restaurantname','pricerange','area','food','phone','addr','postcode'])
+
+            matched_restarent.to_csv(r'raw_files/filtered_hotels.csv',index=False)
         return matched_restarent
     
     def bot_response(self,message):
         message = message.lower()
+        bot_msg=''
         intent=self.textclassfier(message)
-        print(intent)
         keywords=self.keyextractions(message)
-        print(keywords)
         results=self.get_results(message,intent,keywords)
-        # print(results)
         if len(results)>0:
-            response_restarent=results.sample().to_dict()
-            print(response_restarent)
+            selected_restaurant=results.sample(n=1)
+            response_restarent=selected_restaurant.iloc[0].to_dict()
+            selected_restaurant.to_csv(r'raw_files/selected_restaurant.csv',index=False)
+            bot_msg='Would you like to try {}'.format(response_restarent['restaurantname'])
         else:
             response_restarent='empty'
-        return response_restarent
+            results=self.closest_results(keywords)
+            if len(results)==0:
+                bot_msg='Could not find a match wanna look for something else'
+            else:
+                selected_restaurant=results.sample(n=1)
+                response_restarent=selected_restaurant.iloc[0].to_dict()
+                selected_restaurant.to_csv(r'raw_files/selected_restaurant.csv',index=False)
+           
+                if response_restarent['min_dist']=='lv_pricerange': 
+                    bot_msg= 'Did you mean {} Here is a restaurant for the result {}'.format(response_restarent['pricerange'],response_restarent['restaurantname'])
+                if response_restarent['min_dist']=='lv_area':
+                    bot_msg= 'Did you mean {} Here is a restaurant for the result {}'.format(response_restarent['area'],response_restarent['restaurantname'])
+                if response_restarent['min_dist']=='lv_food':
+                    bot_msg= 'Did you mean {} Here is a restaurant for the result {}'.format(response_restarent['food'],response_restarent['restaurantname'])
+        return bot_msg
     def class_bot(self):
         flag= True
         msg = input("Hi I'm a bot is anything i can help you with:")
         while flag:
-            bot_response=self.bot_response(msg)
-            msg= input(bot_response)
-            if msg == 'quit':
+            try:
+                bot_response=self.bot_response(msg)
+                msg= input(bot_response)
+                if msg == 'quit':
+                    if os.path.exists(r'raw_files/selected_restaurant.csv'):
+                        os.remove(r'raw_files/selected_restaurant.csv')
+                    if os.path.exists(r'raw_files/filtered_hotels.csv'):
+                        os.remove(r'raw_files/filtered_hotels.csv')
+                    if os.path.exists(r'raw_files/closest_results.csv'):
+                        os.remove(r'raw_files/closest_results.csv')    
+                    flag= False
+            except:
+                if os.path.exists(r'raw_files/selected_restaurant.csv'):
+                        os.remove(r'raw_files/selected_restaurant.csv')
+                if os.path.exists(r'raw_files/filtered_hotels.csv'):
+                        os.remove(r'raw_files/filtered_hotels.csv')
+                if os.path.exists(r'raw_files/closest_results.csv'):
+                        os.remove(r'raw_files/closest_results.csv')                  
                 flag= False
